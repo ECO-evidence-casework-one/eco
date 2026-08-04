e42fe860ad16f88e1b528a6005efb9745e7e4bcd
Nexlu1 <192837588+Nexlu1@users.noreply.github.com>
2026-08-04 21:26:34 +0100

    Recovery: add desktop application entry point

---
+++ recovery/Eco.Recovery/Program.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+
+namespace Eco.Recovery;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
