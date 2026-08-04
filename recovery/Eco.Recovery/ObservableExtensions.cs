326775b54e1a5b698b800ed051dbaa2c6fb8b6e3
Nexlu1 <192837588+Nexlu1@users.noreply.github.com>
2026-08-04 21:35:56 +0100

    Recovery: add bounded observable action adapter

---
+++ recovery/Eco.Recovery/ObservableExtensions.cs
@@ -0,0 +1,24 @@
+namespace Eco.Recovery;
+
+internal static class ObservableExtensions
+{
+    public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(onNext);
+        return source.Subscribe(new ActionObserver<T>(onNext));
+    }
+
+    private sealed class ActionObserver<T>(Action<T> onNext) : IObserver<T>
+    {
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(T value) => onNext(value);
+    }
+}
